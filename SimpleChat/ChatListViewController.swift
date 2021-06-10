2b7a3f83ad8cd01f8253aeb1bca7056cbf48f03c
tomohiro1024 <gongyezhihong732@gmail.com>
2021-06-10 22:20:42 +0900

    ChatListTableViewCell

--- SimpleChat/ChatListViewController.swift
+++ SimpleChat/ChatListViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class ChatListViewController: UIViewController {
     
+    @IBOutlet weak var chatListTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+}
+
+class ChatListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var latestMesseageLabel: UILabel!
+    
+    @IBOutlet weak var partnerLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        userImageView.layer.cornerRadius = 35
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
     
 }
